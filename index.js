5a793634fc4d779f85c397910b5cf41aa01f349e
Joel Assagou <liamstiduotechnology@gmail.com>
2024-07-26 22:06:10 +0000

    fourth commit

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express = require('express')
 const app = express()
-
-
-
-// morgan.token('req-body',(req) =>  JSON.stringify(req.body))
-// app.use(morgan(':method :url :status :res[content-length] - :response-time ms - Body: :req-body'))
-
+require('dotenv').config()
+const Contact = require('./model/contacts')
 
 
 
@@ -43,95 +39,131 @@ const requestLogger = (request, response, next) => {
     next()
   }
 
-// const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  
+  const errorHandler = (error, request, response, next) => {
+      console.error(error.message)
+  
+    if(error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }else if(error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+    
+    next(error)
+}
+
+
 
 // display all data from array data
 app.get('/api/persons', (request, response) => {
-    try {
-        response.json(data)        
-    } catch (error) {
-        console.log(error);
-    }
+    Contact.find({})
+    .then(contact => response.json(contact))
+    .catch(error => console.log(error))
 })
 
 // display custom data
 app.get('/info', (resquest, response) => {
-    let contactNumber = data.length
-    let date = new Date()
-    response.end(
-        `<p>Phonebook has info for ${contactNumber} persons</p><p>${date}</p>`
-    )
+    Contact.find({}).then(contact => response.status(200).send(`<h3>the phone book has ${contact.length} contacts</h3><h3>${contact.map(time => time.date)}</h3>`))
+    
 })
 
 // display a single phonebook entry
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
-
-    const singleEntry = data.find(entry => entry.id === id)
-
-    if(singleEntry) {
-        response.json(singleEntry)
-    }else{
-        response.status(404).end(`<p>phonebook not found</p>`)
-    }
-
+    
+    Contact.findById(id)
+    .then(contact => {
+        if(contact) {
+            response.json(contact)
+        }else{
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
+    
 })
 
 
 // Delete a single phonebook entry
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id;
-
-    const person = data.filter(entry => entry.id !== id)
-
-    response.status(204).end('contact remove successfully', person)
-    // console.log(person);
+    
+    Contact.findByIdAndDelete(id)
+    .then(contact => response.json(contact))
+    .catch(error => console.log(error))
 }) 
 
 
+// async function updateUserAgeByUsername(username, newNumber) {
+//     try {
+//       const result = await Contact.find({}).then(res => res.forEach(user => user.name))
+      
+//       if (!result) {
+//         console.log('No user found with the given username');
+//       } else {
+//         result.number = newNumber
+//         console.log('User phone number updated successfully');
+//       }
+//     } catch (error) {
+//       console.error('Error updating user age:', error);
+//     }
+//   }
+
+
 // add new person to phonebook
-
-const generateId = () => {
-    const randomNumber = Math.floor(Math.random() * 1000)
-    const phoneId = data.map(num => num.id !== randomNumber) ? randomNumber : randomNumber 
-    return phoneId    
-}
-
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', async (request, response) => {
     const body = request.body
-
+    
     if(!body.name) {
         return response.status(404).json({'error': 'name is missing'})
     }
-
+    
     if(!body.number) {
         return response.status(404).json({'error': 'number is missing'})
     }
-
-    const duplicate = data.find(entry => entry.name === body.name)
-
-    if(duplicate) {return response.status(409).json({'error': 'duplicate name, name must be unique'})}
-
-    const entry = {
-        id: generateId(),
+    
+    
+    const contact = new Contact({
         name: body.name,
         number: body.number
-    }
+    })
 
-    data = data.concat(entry)
+      contact.save().then(newContact => response.json(newContact))
+      .catch(error => console.log(error) )        
     
-    response.json(data)
-
 })
 
 
 
-const PORT = process.env.PORT || 3001
+// UPDATE CONTACT
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    const id = request.params.id
+
+    const updateContact = {
+        name: body.name,
+        number: body.number
+    }
+    
+    Contact.findByIdAndUpdate(id,updateContact, {new: true}).then(contact => response.json(contact))
+    .catch(error => next(error))
+})
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)})
